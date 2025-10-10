ea87d524efa792441299dc9eab4431a7c509a23d
ashiksufaid <ashiksufaid@gmail.com>
2025-10-10 12:44:13 +0530

    Added resnet50 training pipeline to main

---
+++ Dataset.py
@@ -0,0 +1,75 @@
+#cropper
+#The cropped ROIs should have info weather it is mitotic or non mitotic
+
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+import cv2
+import os
+import json
+import numpy as np 
+from PIL import Image, ImageOps
+
+class GliomaDataset(Dataset):
+    def __init__(self, root_dir, target_size=(48,48), do_aug = False):
+        self.root_dir = root_dir
+        self.target_size = target_size
+        self.do_aug = do_aug
+        self.train_transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5),
+            transforms.RandomRotation(degrees=15),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5])
+        ])
+        self.test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5])
+            ])
+            
+        self.images = sorted([f for f in os.listdir(root_dir) if f.endswith("jpg")])
+        self.jsons = sorted([f for f in os.listdir(root_dir) if f.endswith("json")])
+
+        self.samples = []
+        for image, js in zip(self.images, self.jsons):
+            img_path = os.path.join(root_dir, image)
+            json_path = os.path.join(root_dir, js)
+            with open(json_path, "r") as f:
+                data = json.load(f)
+                annotations = data["shapes"]
+        
+            for shape in annotations:
+                points = shape['points']
+                xmin, ymin = np.min(points, axis = 0)
+                xmax, ymax = np.max(points, axis = 0)
+                bbox = [xmin, ymin, xmax, ymax] 
+                label = shape['label']
+                label = 1 if label == "Mitosis" else 0
+                self.samples.append((img_path, bbox, label))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def aspect_ratio_resize_and_pad(self,img):
+        img = img.copy()
+        img.thumbnail(self.target_size, Image.BILINEAR)
+        delta_w = self.target_size[0] - img.width
+        delta_h = self.target_size[1] - img.height
+        padding = (delta_w // 2, delta_h // 2, delta_w - (delta_w // 2), delta_h - (delta_h // 2))
+        img = ImageOps.expand(img, padding, fill=(0, 0, 0))
+        return img
+
+    def __getitem__(self, idx):
+        img_path, bbox, label = self.samples[idx]
+        image = Image.open(img_path).convert("RGB")
+        xmin, ymin, xmax, ymax = map(int, bbox)
+        crop = image.crop((xmin, ymin, xmax, ymax))  # PIL crop: (left, upper, right, lower)
+        crop = self.aspect_ratio_resize_and_pad(crop)
+        if self.do_aug:
+            crop = self.train_transform(crop)
+        else:
+            crop = self.test_transform(crop)
+        label_tensor = torch.tensor([label], dtype=torch.float32)
+        return crop, label_tensor
+        
---
+++ main.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+from Dataset import GliomaDataset
+from torch.utils.data import DataLoader
+from train import train_model
+
+resnet = models.resnet50(pretrained=True)
+
+num_classes = 1
+resnet.fc = nn.Linear(resnet.fc.in_features, num_classes)
+
+for param in resnet.parameters():
+    param.requires_grad = False
+for param in resnet.layer3.parameters():
+    param.requires_grad = True
+for param in resnet.layer4.parameters():
+    param.requires_grad = True
+for param in resnet.fc.parameters():
+    param.requires_grad = True
+
+train_dir = "/home/ashiksufaid/summer proj 2/Data_122824/Glioma_MDC_2025_training"
+train_dataset = GliomaDataset(train_dir, do_aug=True)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle = True)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam
+epochs = 10
+device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
+resnet.train()
+loss = train_model(resnet, epochs, train_loader, criterion, optimizer, device)
